ab0088a9a6f8756c3994dad1edabc65fdfae4435
Aashish Kapur <aashish97+github@gmail.com>
2016-10-09 07:28:00 -0400

    hgf

--- makePipe.py
+++ makePipe.py
@@ -1,25 +1,46 @@
-import os, time, sys
-import errno
+import os, tempfile
 
-pipe_name = 'pipe_test'
-
-def child():
-	# os.mkfifo("")
-	path = "/pipe"
-	# try
-		# os.mkfifo(path)
-	print "path: "
-	print path
-	fifo = open(path, "w")
-	print fifo
-	fifo.write(sys.argv[1] + "," + sys.argv[2] + "," + sys.argv[3])
-	fifo.close()
-    # pipeout = os.open(pipe_name, os.O_WRONLY)
-    # os.write(sys.argv[1] + "," + sys.argv[2] + "," + sys.argv[3])
-    # os.close()
+tmpdir = tempfile.mkdtemp()
+filename = os.path.join(tmpdir, 'pipe')
+print filename
+try:
+    os.mkfifo(filename)
+except OSError, e:
+    print "Failed to create FIFO: %s" % e
+else:
+    fifo = open(filename, 'w')
+    # write stuff to fifo
+    print >> fifo, "hello"
+    fifo.close()
+    os.remove(filename)
+    os.rmdir(tmpdir)
 
 
-child()    
+
+
+
+# import os, time, sys
+# import errno
+
+# pipe_name = 'pipe_test'
+
+# def child():
+# 	# os.mkfifo("")
+# 	path = "/pipe"
+# 	# try
+# 		# os.mkfifo(path)
+# 	print "path: "
+# 	print path
+# 	fifo = open(path, "w")
+# 	print fifo
+# 	fifo.write(sys.argv[1] + "," + sys.argv[2] + "," + sys.argv[3])
+# 	fifo.close()
+#     # pipeout = os.open(pipe_name, os.O_WRONLY)
+#     # os.write(sys.argv[1] + "," + sys.argv[2] + "," + sys.argv[3])
+#     # os.close()
+
+
+# child()    
 
 
 # import os
