6b27cf436255b5d6d974ec3773ec4cc329810b04
Aashish Kapur <aashish97+github@gmail.com>
2016-10-09 07:42:28 -0400

    hgf

--- makePipe.py
+++ makePipe.py
@@ -7,13 +7,11 @@ try:
     os.mkfifo(filename)
 except OSError, e:
     print "Failed to create FIFO: %s" % e
-else:
-    fifo = open(filename, 'w')
+fifo = open(filename, 'w')
     # write stuff to fifo
-    print >> fifo, "hello"
-    fifo.close()
-    os.remove(filename)
-    os.rmdir(tmpdir)
+print >> fifo, "hello"
+fifo.close()
+# os.remove(filename)
 
 
 
